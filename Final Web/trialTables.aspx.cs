50f6d9d27e576799ee37703ae0083ae915e298ff
123wandola.h <123wandola.h@gmail.com>
2019-05-12 14:52:03 +0300

    Something just before i accidentally deleted a web page lool

--- Final Web/trialTables.aspx.cs
+++ Final Web/trialTables.aspx.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Final_Web
 {
-    public partial class trialTables : System.Web.UI.Page
+    public partial class TrialTables : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- Final Web/trialTables.aspx.designer.cs
+++ Final Web/trialTables.aspx.designer.cs
@@ -10,6 +10,15 @@
 namespace Final_Web {
     
     
-    public partial class trialTables {
+    public partial class TrialTables {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
