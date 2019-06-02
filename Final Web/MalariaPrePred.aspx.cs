d36b742087906b2dc5bc2d67e7e3dd3d26de3fe4
123wandola.h <123wandola.h@gmail.com>
2019-06-02 17:08:24 +0300

    Still working on it

--- Final Web/MalariaPrePred.aspx.cs
+++ Final Web/MalariaPrePred.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Final_Web
 {
-    public partial class MalariaPrePred : Page
+    public partial class MalariaPrePred : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
